46083a968cd8338fc86389da1b45319cac70bf1c
Evgeny <evgeny.she+git@gmail.com>
2015-03-04 18:03:54 +0200

    Update calculator.py
    Inplace validation of input instead of many try-except blocks

--- calculator.py
+++ calculator.py
@@ -58,83 +58,66 @@ def profit(sales, pay_used, cost_sales):
     return gain
 
 
+def float_input(text):
+    while True:
+        value = input(text + ' ')
+        if value.upper() == 'QUIT':
+            quit()
+        try:
+            return float(value)
+        except ValueError:
+            print("OOPS! That wasn't a number!")
+
+
 # Allow user to choose which calculation they would like to perform
 def choose():
         print(" ")
         print(" ")
         choice = input("What would you like me to calculate? Or type 'quit' to quit." + "\n>>>")
         if choice.upper() == "MPG":
-            try:
-                traveled = float(input("How many miles were traveled?"))
-                fuel = float(input("How many gallons of fuel were used?"))
-            except ValueError:
-                print("OOPS! That wasn't a number!")
-                choose()
+            traveled = float_input("How many miles were traveled?")
+            fuel = float_input("How many gallons of fuel were used?")
             miles_per_gallon = mileage(traveled, fuel)
             print("After traveling " + str(traveled) + " miles and using " + str(fuel) + " gallons of fuel. Your MPG was " + str(miles_per_gallon))
             choose()
 
         elif choice.upper() == "KINETIC":
-            try:
-                weight = float(input("How much does the object weight (in kilograms)?"))
-                speed = float(input("What is the speed of the object (in meters per second)?"))
-            except ValueError:
-                print("OOPS! That wasn't a number!")
-                choose()
+            weight = float_input("How much does the object weight (in kilograms)?")
+            speed = float_input("What is the speed of the object (in meters per second)?")
             energy = kineteic_energy(weight, speed)
             print("The kinetic energy of an object weighing " + str(weight) + " kilograms and moving " + str(speed) + " meters per second is " + str(energy) + " joules.")
             choose()
 
         elif choice.upper() == "PAY":
-            try:
-                hours_worked = float(input("How many hours were worked?"))
-                pay_rate = float(input("What is the pay rate per hour?"))
-            except ValueError:
-                print("OOPS! That wasn't a number!")
-                choose()
+            hours_worked = float_input("How many hours were worked?")
+            pay_rate = float_input("What is the pay rate per hour?")
             pay_check = pay(hours_worked, pay_rate)
             print("Working for " + str(hours_worked) + " hours at " + str(pay_rate) + " per hour. Your pay is " + str(pay_check) + " dollars.")
             choose()
 
         elif choice.upper() == "FTM":
-            try:
-                foot = float(input("How many feet would would you like to convert?"))
-            except ValueError:
-                print("OOPS! That wasn't a number!")
-                choose()
+            foot = float_input("How many feet would would you like to convert?")
             meter = feet_to_meters(foot)
             print(str(foot) + " feet is " + str(meter) + " meters.")
             choose()
 
         elif choice.upper() == "YIELD":
-            try:
-                sales = float(input("How much is money was brought in(sales)?"))
-                hours = float(input("How many labor hours were used?"))
-            except ValueError:
-                print("OOPS! That wasn't a number!")
-                choose()
+            sales = float_input("How much is money was brought in(sales)?")
+            hours = float_input("How many labor hours were used?")
             labor_y = labor_yield(sales, hours)
             print("For " + str(sales) + " in sales and " + str(hours) + " hours of labor used, labor yield is " + str(labor_y) + " dollars.")
             choose()
 
         elif choice.upper() == "PERCENT":
-            try:
-                sales = float(input("How much money was brought in(sales)?"))
-                payroll = float(input("How much money was spent on labor?"))
-            except ValueError:
-                print("OOPS! That wasn't a number!")
-                choose()
+            sales = float_input("How much money was brought in(sales)?")
+            payroll = float_input("How much money was spent on labor?")
             print("For " + str(sales) + " dollars in sales and " + str(payroll) + " dollars spent on labor, labor is " + str(labor_percent(sales, payroll)) + " percent.")
             choose()
 
         elif choice.upper() == "PROFIT":
-            try:
-                sales = float(input("How much money was brought in(sales)?"))
-                payroll = float(input("How much money was spent on labor?"))
-                cost_of_sales = float(input("How much did the merchandise cost?"))
-            except ValueError:
-                print("OOPS! That wasn't a number!")
-                choose()
+            sales = float_input("How much money was brought in(sales)?")
+            payroll = float_input("How much money was spent on labor?")
+            cost_of_sales = float_input("How much did the merchandise cost?")
             profits = profit(sales, payroll, cost_of_sales)
             print("For " + str(sales) + " dollars in sales, " + str(payroll) + " dollars spent on labor and " + str(cost_of_sales) + " dollars spent on merchandise, profits are " + str(profits) + " dollars.")
             choose()
@@ -147,6 +130,3 @@ def choose():
             choose()
 
 choose()
-
-
-
